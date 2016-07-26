4fcb3d6df7dc7c2f5805bd734153df75364640a0
Stuart Frost <stuart.frosty@gmail.com>
2016-07-26 12:53:51 +0100

    add bootstrap navbar, home & tools controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
     root 'home#index'
+    resources :tools
+  # get 'scales', :to => 'home#tools'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
