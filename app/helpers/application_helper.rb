2504f271375b83550db1c229d051360b129d7c67
Stuart Frost <stuart.frosty@gmail.com>
2016-07-29 22:13:33 +0100

    prettify dates/times and inputs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def format_date date, day
+    day ? date.strftime("%a %d-%m-%y") : date.strftime("%d-%m-%y")
+  end
+
+  def format_time time
+    time.strftime("%H:%M")
+  end
 end
