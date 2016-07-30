a8a076083425cb8d88c3060df0e71b169cc99068
Stuart Frost <stuart.frosty@gmail.com>
2016-07-30 18:40:36 +0100

    refactor stats facade

--- app/facades/stats_facade.rb
+++ app/facades/stats_facade.rb
@@ -5,16 +5,10 @@ class StatsFacade
     @logs = logs
   end
 
-  def this_week
-    @logs.where(date: Date.today.at_beginning_of_week..Date.today.at_end_of_week)
-  end
-
-  def last_week
-    @logs.where(date: 1.week.ago.at_beginning_of_week..1.week.ago.at_end_of_week)
-  end
-
-  def hours_practiced logs_from_week
+  def hours_practiced date
+    logs_from_week = @logs.where(date: date.at_beginning_of_week..date.at_end_of_week)
     result = 0
+
     logs_from_week.each do |log|
       result += ((log.time_to - log.time_from) / 1.hour) 
     end
