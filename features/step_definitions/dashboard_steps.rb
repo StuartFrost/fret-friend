e338a2af43081ac73fb28752400a4230821268d3
Stuart Frost <stuart.frosty@gmail.com>
2016-07-30 09:46:49 +0100

    make feature useful

--- features/step_definitions/dashboard_steps.rb
+++ features/step_definitions/dashboard_steps.rb
@@ -1,6 +1,6 @@
 Given(/^there are some entries in the database$/) do
-  @log1 = create :practice_log
-  @log2 = create :practice_log
+  @log1 = create :practice_log, time_from: 1.hour.ago, time_to: DateTime.now
+  @log2 = create :practice_log, time_from: 2.hours.ago, time_to: DateTime.now
 end
 
 When(/^I visit the dashboard page$/) do
@@ -8,5 +8,5 @@ When(/^I visit the dashboard page$/) do
 end
 
 Then(/^I should see the number of hours practiced this week$/) do
-  expect(page).to have_content "practiced this week"
+  expect(page).to have_content "3.0"
 end
