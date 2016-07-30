4a8ddb427f22c8f259473db3d5118d30d3b552ca
Stuart Frost <stuart.frosty@gmail.com>
2016-07-30 09:52:54 +0100

    failing feature - view last week hours

--- features/dashboard.feature
+++ features/dashboard.feature
@@ -4,3 +4,9 @@ Feature: Dashboard
     Given there are some entries in the database
     When I visit the dashboard page
     Then I should see the number of hours practiced this week
+
+    @wip
+  Scenario: Viewing hours practiced last week
+    Given there are some entries from last week
+    When I visit the dashboard page
+    Then I should see the number of hours practiced last week
--- features/step_definitions/dashboard_steps.rb
+++ features/step_definitions/dashboard_steps.rb
@@ -3,10 +3,19 @@ Given(/^there are some entries in the database$/) do
   @log2 = create :practice_log, time_from: 2.hours.ago, time_to: DateTime.now
 end
 
+Given(/^there are some entries from last week$/) do
+  @log1 = create :practice_log, date: 1.week.ago, time_from: 3.hour.ago, time_to: DateTime.now
+  @log2 = create :practice_log, date: 1.week.ago, time_from: 2.hours.ago, time_to: DateTime.now
+end
+
 When(/^I visit the dashboard page$/) do
   visit root_path
 end
 
 Then(/^I should see the number of hours practiced this week$/) do
   expect(page).to have_content "3.0"
+end
+
+Then(/^I should see the number of hours practiced last week$/) do
+  expect(page).to have_content "5.0"
 end
