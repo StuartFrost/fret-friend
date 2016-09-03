162183d72b0ce9539c070ae3be57d899e365b31d
Stuart Frost <stuart.frosty@gmail.com>
2016-09-03 10:01:24 +0100

    improve practice log factory & facade tests

--- factories/practice_log_factory.rb
+++ factories/practice_log_factory.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :practice_log do
     date { Faker::Date.between(7.days.ago, Date.today) }
-    time_from { Faker::Time.between(DateTime.now - 2.hours, DateTime.now) }
-    time_to { Faker::Time.between(DateTime.now, DateTime.now) }
+    time_from { Faker::Time.forward(0, :morning) }
+    time_to { Faker::Time.forward(0, :afternoon) }
     practiced { Faker::ChuckNorris.fact }
   end
 end
--- spec/facades/stats_facade_spec.rb
+++ spec/facades/stats_facade_spec.rb
@@ -14,10 +14,16 @@ describe StatsFacade do
   describe "hours_practiced_per_day" do
     it "returns a hash containing the number of hours practiced for each week within the date range" do
       FactoryGirl.create :practice_log, date: Date.today, time_from: "19:00", time_to: "21:00"
+      FactoryGirl.create :practice_log, date: Date.today - 2.weeks, time_from: "18:00", time_to: "21:00"
       facade = StatsFacade.new PracticeLog.all
 
-      result = {Date.today.at_beginning_of_week => 2}
-      expect(facade.hours_practiced_per_week(Date.today - 1.day)).to eq result
+      week = Date.today.at_beginning_of_week
+      result = {week - 4.weeks => 0.0,
+                week - 3.weeks => 0.0,
+                week - 2.weeks => 3.0,
+                week - 1.weeks => 0.0,
+                week => 2.0}
+      expect(facade.hours_practiced_per_week(Date.today - 1.month)).to eq result
     end
   end
 end
